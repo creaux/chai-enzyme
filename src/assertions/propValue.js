f5482548bde38e2c2067332b3afde570675db41b
Petr Juna <petr.juna@nestle.com>
2017-04-03 18:00:35 +0200

    support for propValue

--- src/assertions/propValue.js
+++ src/assertions/propValue.js
@@ -1,11 +1,10 @@
-export default function ref ({ wrapper, markup, flag, arg1, arg2, sig }) {
-    const actual = wrapper.prop();
-
+export default function ref (props) {
+    console.log('H', props);
+    const { markup, arg1, sig } = props;
     this.assert(
-        arg1.indexOf(arg2) > -1,
+        wrapper.prop(arg1[0]).indexOf(arg1[1]) > -1,
         () => 'expected ' + sig + ' to have propValue #{exp} but props were #{act} ' + markup(),
         () => 'expected ' + sig + ' not to have propValue #{exp} but props were #{act} ' + markup(),
         arg1,
-        Object.keys(actual)
     );
 }
